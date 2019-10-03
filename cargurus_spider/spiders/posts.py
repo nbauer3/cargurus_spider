a87d7a673753ac659a97c271f362c417fa0a9d69
Nick <Bauer_Nicholas@bah.com>
2019-10-03 01:07:32 -0400

    expanding post search & updated UI

--- cargurus_spider/spiders/posts.py
+++ cargurus_spider/spiders/posts.py
@@ -8,14 +8,38 @@ class PostsSpider(scrapy.Spider):
     start_urls = ['http://www.cargurus.com/Cars/inventorylisting/viewDetailsFilterViewInventoryListing.action?sourceContext=carGurusHomePageModel&entitySelectingHelper.selectedEntity=d2207&zip=20164/']
 
     def parse(self, response):
+    	title_0 = response.xpath('//h1/text()').extract()[0]
+    	title_1 = response.xpath('//h1/em/text()').extract()[0]
+    	title_2 = response.xpath('//h1/text()').extract()[1]
+    	title_3 = str(response.xpath('//h1/em[2]/text()').extract_first())
+    	# title location coming back as 'Nationwide'
+    	title = title_0 + title_1 + title_2 + title_3
+
+    	print '\n\n#################### - STARTING SPIDER - ####################\n\n'
+        print title
+
     	# returns for all cars: car listing - 'Used Cars' - location
     	# response.xpath('//*[@class="cg-dealFinder-result-model"]/span/text()').extract()
         posts = response.xpath('//*[@class="cg-dealFinder-result-model"]')
         i = 0
-        for post in posts:
+
+        #45 bc there are 45 /spans
+        while i < 45:
         	# some funky code print post #
+        	#if i % 0 == 0 or i % 1 == 0:
+        	# TODO h4/span/text???
         	info = response.xpath('.//*[@class="cg-dealFinder-result-model"]/span/text()').extract()[i]
         	if i % 3 == 0:
-        		print 'Post #' + str(i/3)
+        		print '\n-------------------------------------------------------------\n'
+        		print 'Post #' + str(1 + (i/3)) # LOL
         	print info
         	i += 1
+
+        print '\n\n##################### - ENDING SPIDER - #####################\n\n'
+        #next_page_url = response.xpath('//*[@class="next"]/a/@href').extract_first()
+     	#absolute_next_page_url = response.urljoin(next_page_url)
+     	# doest work bc adds '/' but also hardcoded the url into scrapy.Request
+     	# absolute_next_page_url = response.urljoin('#resultsPage=2')
+
+     	# hard coding new url doesnt work
+     	#yield scrapy.Request('https://www.cargurus.com/Cars/inventorylisting/viewDetailsFilterViewInventoryListing.action?sourceContext=carGurusHomePageModel&entitySelectingHelper.selectedEntity=d2207&zip=20164#resultsPage=2')
